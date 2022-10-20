805a3bb944b920b733806f488ef9318f1365ada3
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-10-20 19:06:26 -0400

    thought model

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = newSchema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (data) => {
+        return new Date(date).toLocaleString();
+      }
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJson: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
