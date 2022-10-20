51c06e2776c528f947eccde6974affee02451b44
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-10-20 19:44:22 -0400

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,8 @@
-const { connect, connection } = require('mongoose');
-const con = process.env.MONGODB_URI || 'mongo://127.0.0.1:27017/SNApiDB';
+const mongoose = require('mongoose');
 
-connect(con, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/SNApiDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-module.exports = connection;
+module.exports = mongoose.connection;
