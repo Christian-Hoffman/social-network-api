b73c966005098f82cdc48d26ed572b0a3c7d1787
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-10-20 19:12:57 -0400

    reaction model, added to thought

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const { Schema, model, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      max: 280
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.noe,
+      get: (date) => {
+        return new Date(date).toLocaleString();
+      },
+    }
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
